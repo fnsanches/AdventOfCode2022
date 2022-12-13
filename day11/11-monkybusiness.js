60fc8d5a4e9478c1adb569428901150c5535e49e
fnsanches <felinobre100@gmail.com>
2022-12-13 11:12:51 -0300

    day11

---
+++ day11/11-monkybusiness.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+var input = fs.readFileSync('input.txt','utf8');
+const data = input.replace(/\r?\n|\r/g, '\n')
+
+function part1(input, count = 20) {
+    const monkeys = input.split('\n\n').map(monkey => {
+      const lines = monkey.split('\n');
+      const [, items] = lines[1].split(': ');
+      const [safe] = lines[2].match(/(old|\d+) (\*|\+) (old|\d+)$/);
+      return {
+        items: items.split(', ').map(x => +x),
+        operation: new Function('old', `return ${safe}`),
+        divisible: +lines[3].match(/\d+$/)[0],
+        yes: +lines[4].match(/\d+$/)[0],
+        no: +lines[5].match(/\d+$/)[0],
+        business: 0,
+      };
+    });
+    const base = monkeys.reduce((prev, monkey) => prev * monkey.divisible, 1);
+    for (let i = 0; i < count; i++) {
+      for (const monkey of monkeys) {
+        while (monkey.items.length > 0) {
+          let worry = monkey.operation(monkey.items.shift());
+          worry = count > 20 ? worry % base : Math.floor(worry / 3);
+          const target = worry % monkey.divisible === 0 ? monkey.yes : monkey.no;
+          monkeys[target].items.push(worry);
+          monkey.business++;
+          // console.log(monkey)
+        }
+      }
+    }
+    const [top1, top2] = monkeys.sort((a, b) => b.business - a.business);
+    return top1.business * top2.business;
+  }
+
+  console.log(part1(data))
+//   console.log(part1(data, 10000))
